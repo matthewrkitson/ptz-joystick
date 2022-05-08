00474c9204c896be0e31322b69bba5dc0143662d
Matthew Kitson <[200~matthewrkitson@users.noreply.github.com>
2022-05-08 10:46:16 +0100

    Added kepyad experiment

--- experiments/keypad.py
+++ experiments/keypad.py
@@ -1,20 +1,43 @@
+import functools
 import gpiozero
 import signal
 
-col1 = gpiozero.LED(26)
-col2 = gpiozero.LED(19)
-col3 = gpiozero.LED(13)
+col1 = gpiozero.LED(19)
+col2 = gpiozero.LED(13)
+col3 = gpiozero.LED( 6)
 
-row1 = gpiozero.Button(6)
-row2 = gpiozero.Button(5)
-row3 = gpiozero.Button(16)
-row4 = gpiozero.Button(12)
+row1 = gpiozero.Button( 5,  pull_up=False)
+row2 = gpiozero.Button(11,  pull_up=False)
+row3 = gpiozero.Button( 9, pull_up=False)
+row4 = gpiozero.Button(10, pull_up=False)
 
 columns = (col1, col2, col3)
 rows = (row1, row2, row3, row4)
+keys = (
+    ( 1,  2,  3),
+    ( 4,  5,  6),
+    ( 7,  8,  9),
+    ("*", 0, "#")
+)
 
-def column_pressed(column):
-    print(f"Column pressed {column}")
+def button_pressed(row_index, column_index):
+    print(f"{keys[row_index][column_index]} ({column_index}, {row_index})")
+
+def row_pressed(row_index, row):
+    if not row.is_active: return
+
+    for column_index, column in enumerate(columns):
+        column.off()
+        if not row.is_active:
+            button_pressed(row_index, column_index)
+        column.on()
 
 for column in columns:
-    column.when_pressed = column_pressed
+    column.on()
+
+for index, row in enumerate(rows):
+    # Assign to local variable to avoid closing over index.
+    # row.when_pressed = lambda row: row_pressed(row, local_index), 
+    row.when_pressed = functools.partial(row_pressed, index)
+
+signal.pause()
