fdca1786873075a463874ddd3047889d5275468c
Matthew Kitson <[200~matthewrkitson@users.noreply.github.com>
2022-05-15 15:33:22 +0100

    Focusser (mostly) implemented

---
+++ software/focusser.py
@@ -0,0 +1,31 @@
+class Focusser(d):
+    def __init__(self, focus_in_button, focus_out_button, focus_auto_button, camera, lcd):
+        self.focus_in_button = focus_in_button
+        self.focus_out_button = focus_out_button
+        self.focus_auto_button = focus_auto_button
+        self.camera = camera
+        self.lcd = lcd
+
+        self.focus_in_button.when_pressed = self.focus_in
+        self.focus_in_button.when_released = self.focus_stop
+
+        self.focus_out_button.when_pressed = self.focus_out
+        self.focus_out_button.when_released = self.focus_stop
+
+        self.focus_auto_button.when_pressed = self.focus_auto
+
+    def focus_in(self):
+        self.lcd.print_line1("Focus in")
+        self.camera.focus_in(1)
+
+    def focus_out(self):
+        self.lcd.print_line1("Focus out")
+        self.camera.focus_out(1)
+
+    def focus_stop(self):
+        self.lcd.print_line1("Focus stop")
+        self.camera.focus_stop()
+
+    def focus_auto(self):
+        self.lcd.print_line1("Auto focus??")
+        # self.camera.auto_focus()
--- software/joystick.py
+++ software/joystick.py
@@ -1,34 +1,32 @@
 import functools
-import gpiozero
 import logging
 import logging.handlers
 import subprocess
 
+import board
+import digitalio
+import adafruit_aw9523
+
 from illuminatedbutton import IlluminatedButton
 from lcd1602rgb import RGB1602
 from quitter import Quitter
+from zoomer import Zoomer
 
 import web_api
 
-def preset_recall(controller, lcd):
+def preset_recall(camera, lcd):
     lcd.print_line1("Preset recall")
 
-def preset_store(controller, lcd):
+def preset_store(camera, lcd):
     lcd.print_line1("Preset store")
 
-def zoom_in(controller, lcd):
-    lcd.print_line1("Zoom in")
-
-def zoom_out(controller, lcd):
-    lcd.print_line1("Zoom out")
-
-def focus_in(controller, lcd):
+def focus_in(camera, lcd):
     lcd.print_line1("Focus in")
 
-def focus_out(controller, lcd):
+def focus_out(camera, lcd):
     lcd.print_line1("Focus out")
 
-def focus_auto(controller, lcd):
+def focus_auto(camera, lcd):
     lcd.print_line1("Auto focus")
 
 logger = logging.getLogger(__name__)
@@ -44,37 +42,38 @@ consoleHandler.setFormatter(formatter)
 fileHandler.setFormatter(formatter)
 
 logger.setLevel(logging.DEBUG)
-logger.info("PTZ joystick controller starting")
+logger.info("PTZ joystick camera starting")
 
-controller = web_api.WebApiController("localhost:8080", logger)
+camera = web_api.WebApiController("localhost:8080", logger)
 
 network_info = subprocess.check_output(["ip", "address"], text=True)
 logger.debug(f"Network status:\n{network_info}")
 
 lcd = RGB1602(16,2)
 lcd.setRGB(64, 128, 64)
-lcd.print_line1(f"PTZ Controller")
+lcd.print_line1(f"PTZ camera")
 
-preset_recall_button = IlluminatedButton(14,    7)
-preset_store_button  = IlluminatedButton(15,   12)
-zoom_in_button       = IlluminatedButton(18,   16)
-zoom_out_button      = IlluminatedButton(23,   26)
-focus_in_button      = IlluminatedButton(24, None)
-focus_auto_button    = IlluminatedButton(25, None)
-focus_out_button     = IlluminatedButton( 8, None)
+i2c = board.I2C()
+aw9523 = adafruit_aw9523.AW9523(i2c)
+
+preset_recall_button = IlluminatedButton(14, aw9523, 1)
+preset_store_button  = IlluminatedButton(15, aw9523, 2)
+zoom_in_button       = IlluminatedButton(18, aw9523, 3)
+zoom_out_button      = IlluminatedButton(23, aw9523, 4)
+focus_in_button      = IlluminatedButton(24, aw9523, 5)
+focus_auto_button    = IlluminatedButton(25, aw9523, 6)
+focus_out_button     = IlluminatedButton( 8, aw9523, 7)
 
 quitter = Quitter(preset_recall_button, preset_store_button, lcd)
+zoomer = Zoomer(zoom_in_button, zoom_out_button, camera, lcd)
 
-preset_recall_button.when_pressed = functools.partial(preset_recall, controller, lcd)
-preset_store_button.when_pressed = functools.partial(preset_store, controller, lcd)
+preset_recall_button.when_pressed = functools.partial(preset_recall, camera, lcd)
+preset_store_button.when_pressed = functools.partial(preset_store, camera, lcd)
 
-zoom_in_button.when_pressed = functools.partial(zoom_in, controller, lcd)
-zoom_out_button.when_pressed = functools.partial(zoom_out, controller, lcd)
-
-focus_in_button.when_pressed = functools.partial(focus_in, controller, lcd)
-focus_out_button.when_pressed = functools.partial(focus_out, controller, lcd)
-focus_auto_button.when_pressed = functools.partial(focus_auto, controller, lcd)
+focus_in_button.when_pressed = functools.partial(focus_in, camera, lcd)
+focus_out_button.when_pressed = functools.partial(focus_out, camera, lcd)
+focus_auto_button.when_pressed = functools.partial(focus_auto, camera, lcd)
 
 quitter.wait_for_exit()
 
-logger.info("PTZ joystick controller stopping")
+logger.info("PTZ joystick camera stopping")
