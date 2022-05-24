108b650416b04efbe856203d52859b1e34def3ec
Matthew Kitson <[200~matthewrkitson@users.noreply.github.com>
2022-05-24 18:44:40 +0100

    Added i2c lock for aw9523

--- software/illuminatedbutton.py
+++ software/illuminatedbutton.py
@@ -1,16 +1,20 @@
 import gpiozero
 
 class IlluminatedButton(gpiozero.Button):
-    def __init__(self, button_pin, aw9523, led_id):
+    def __init__(self, button_pin, aw9523, led_id, i2c_lock):
         gpiozero.Button.__init__(self, button_pin)
         self.aw9523 = aw9523
         self.led_id = led_id
+        self.i2c_lock = i2c_lock
 
-        self.aw9523.LED_modes = aw9523.LED_modes | 1 << led_id
-        self.aw9523.set_constant_current(led_id, 0)
+        with self.i2c_lock:
+            self.aw9523.LED_modes = aw9523.LED_modes | 1 << led_id
+            self.aw9523.set_constant_current(led_id, 0)
 
     def on(self):
-        self.aw9523.set_constant_current(self.led_id, 255)
+        with self.i2c_lock:
+            self.aw9523.set_constant_current(self.led_id, 255)
 
     def off(self):
-        self.aw9523.set_constant_current(self.led_id, 255)
+        with self.i2c_lock:
+            self.aw9523.set_constant_current(self.led_id, 255)
